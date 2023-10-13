b9263051459d674eaa8b988cb85b03feec92d43c
Balaguru <balagurusiva2002@gmail.com>
2023-10-13 12:23:57 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/components/Hero.jsx 	modified:   src/components/Loader.jsx 	modified:   src/components/Navbar.jsx 	modified:   src/components/canvas/Computers.jsx 	modified:   vite.config.js

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,11 +6,11 @@ import {ComputersCanvas} from './canvas'
 
 const Hero = () => {
   return (
-    <section className="relative w-full h-screen mx-auto" >
-      <div className='`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5'>
+    <section className="relative w-full h-screen   mx-auto" >
+      <div className='`${styles.paddingX}` absolute inset-0  md:top-[60px]  top-[90px] max-w-7xl mx-auto flex flex-row items-start gap-5'>
         <div className="flex flex-col justify-center items-center mt-5">
           <div className='w-5 h-5 rounded-full bg-[#915eff]'/>
-          <div className="w-1 sm:h-80 h-80 violet-gradient" />
+          <div className="w-1 sm:h-90 h-80 violet-gradient" />
         </div>
 
         <div>
@@ -18,9 +18,7 @@ const Hero = () => {
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>I develope single page web application <b className='sm:block hidden'/> using  <span className='text-[#915eff]'>MERN stack</span></p>
         </div>
       </div>
-       <ComputersCanvas />
-
-       
+       <ComputersCanvas /> 
     </section>
   )
 }
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import { Suspense, useEffect, useState } from "react"
+import { Canvas } from "@react-three/fiber"
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei"
+import { SpotLight } from "@react-three/drei"
+import canvasLoader from '../Loader'
+import { services } from "../../constants"
+ 
 
-const Computers = () => {
-  return (
-    <div>Computers</div>
-  )
+const Computers = ({isMobile}) => {
+    const computer = useGLTF('./desktop_pc/scene.gltf')
+    return (
+        <mesh>
+            <hemisphereLight intensity={0.15} groundColor="black" />
+            <pointLight intensity={10} />
+            <spotLight
+               position={[-20,50,10]}
+               angle={0.12}
+               penumbra={1}
+               intensity={1}
+               castShadow
+               shadow-mapsize={1024} />
+                
+            
+            <primitive 
+                object={computer.scene}
+                scale={isMobile?0.7:0.75}
+                position={ isMobile ? [0,-1.90,-2.2]:[0, -2.90, -1.2, -0.1]}
+                rotation={[-0.01,-0.2,-0.1]} />
+        </mesh>
+    )
 }
 
-export default Computers
+const ComputersCanvas = () => {
+    const [isMobile , setIsMobile] = useState(false)
+
+    useEffect(() =>{
+        const mediaQuery = window.matchMedia('(max-width:500px)');
+        setIsMobile(mediaQuery.matches)
+
+        const handleMediaQuerychanges =(event) =>{
+            setIsMobile(event.matches)
+        }
+
+        mediaQuery.addEventListener('change', handleMediaQuerychanges)
+
+        return () =>{
+            mediaQuery.removeEventListener('change', handleMediaQuerychanges)
+        }
+     },[])
+    return (
+        <>
+            <Canvas
+                frameloop="demand"
+                shadows
+                camera={{ position: [20, 3, 5], fov: 25 }}
+                gl={{ preserveDrawingBuffer: true }}
+            >
+                <Suspense>
+                    <OrbitControls
+                        enableZoom={false}
+                        maxPolarAngle={Math.PI / 2}
+                        minPolarAngle={Math.PI / 2}
+                    />
+                    <Computers isMobile={isMobile} />
+                </Suspense>
+
+                <Preload all />
+            </Canvas>
+        </>
+
+    )
+}
+
+export default ComputersCanvas
