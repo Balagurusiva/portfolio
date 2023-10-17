957e966f21157237ef61991a2f74fce80d8a9ff6
Balaguru <balagurusiva2002@gmail.com>
2023-10-17 10:41:48 +0530

    modified:   src/components/Tech.jsx 	deleted:    src/components/canvas/Ball.jsx

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,19 +1,28 @@
 import React from 'react'
-import {BallCanvas} from './canvas'
-import {SectionWrapper} from '../HOC'
+import { SectionWrapper } from '../HOC'
 import { technologies } from '../constants'
+import { motion } from "framer-motion"
+import { textVariant } from "../utils/motion"
+
+import { styles } from '../styles'
 
 
 const Tech = () => {
-  return (
-    <div className='flex flex-row flex-wrap justify-center gap-10'>
-        {technologies.map((technology) =>(
-            <div className="w-28 h-28" key={technology.name}>
-                <BallCanvas icons={technology.icon} />
-            </div>
-        ))}
-    </div>
-  )
+    return (
+        <div className='flex flex-row flex-wrap justify-center gap-10'>
+            {technologies.map((technology) => (
+                <div className="w-28 h-28 p-2 m-1" key={technology.name}>
+                    <>
+                        <img src={technology.icon} alt={technology.namr} />
+                        <motion.div variants={textVariant()}>
+                            <p className={`${styles.sectionSubText} text-center text-[16px]`}>{technology.name}</p>
+                        </motion.div>
+                    </>
+
+                </div>
+            ))}
+        </div>
+    )
 }
 
 export default SectionWrapper(Tech, "")
